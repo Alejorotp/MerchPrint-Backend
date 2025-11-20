6a84dd297cca6e2413145c21d0038f3589319568
Alejandro Toro <alejandropinedo@uninorte.edu.co>
2025-11-20 13:51:01 -0500

    Accepting different origins

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -5,7 +5,7 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(ApiGatewayModule);
   app.enableCors({
-    origin: 'http://localhost:3005',
+    origin: true,
     credentials: true,
   });
   await app.init();
